64eb89979f47d08ef37c72e598cdf342a603f71d
Pawan Verma <pawanzofficial@gmail.com>
2023-06-17 17:47:43 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import express from "express";
 import { db, connectionToDB } from "./db.js";
 import admin from "firebase-admin";
+import path from "path";
 import "dotenv/config";
-import cors from "cors";
+import { fileURLToPath } from "url";
 import { uid } from "uid/secure";
 import { createServer } from "http";
 import { Server } from "socket.io";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(express.static(path.join(__dirname, "./build")));
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
