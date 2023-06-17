cf3efa860751f1c18e7fa791fb1b7a5b8f09bec2
Pawan Verma <pawanzofficial@gmail.com>
2023-06-17 18:13:42 +0530

    initial commit

--- db.js
+++ db.js
@@ -3,7 +3,9 @@ import { MongoClient } from "mongodb";
 let db;
 
 async function connectionToDB(cb) {
-	const client = new MongoClient("mongodb://127.0.0.1:27017/");
+	const client = new MongoClient(
+		`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.jckonjc.mongodb.net/?retryWrites=true&w=majority`
+	);
 	await client.connect();
 	db = client.db("chatify");
 	cb();
